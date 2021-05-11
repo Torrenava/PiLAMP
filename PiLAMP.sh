fc44fc71aef48fa61867d0906703f5e9bf98882d
Torrenava <51089658+Torrenava@users.noreply.github.com>
2021-05-11 12:05:24 +0200

    Update PiLAMP.sh

--- PiLAMP.sh
+++ PiLAMP.sh
@@ -28,22 +28,25 @@ function helpPanel(){
 
 function error() {
   echo -e "\n${redColour}[!]${endColour}${redColour} Ha ocurrido un eror.${endColour}"
-  sleep 2; exit 1;
+  sleep 2; exit 1; tput cnorm
 }
 
 function updateRaspian(){
 	clear
+	tput civis
   echo -e "\n${yellowColour}[*]${endColour}${grayColour} Actualizando${endColour}${purpleColour} RasperryPi${endColour}${grayColour}.${endColour}"
   sleep 2
   apt update > /dev/null 2>/dev/null
   apt upgrade -y > /dev/null 2>/dev/null
   echo -e "\n${yellowColour}[*]${endColour}${grayColour} Actualización Correcta.${endColour}"
   sleep 2
+	tput cnorm
 
 }
 
 function installApache() {
   clear
+	tput civis
   echo -e "\n${yellowColour}[*]${endColour}${grayColour} Instalando Apache.${endColour}"
   sleep 2
   apt install -y apache2 > /dev/null 2>/dev/null
@@ -54,12 +57,14 @@ function installApache() {
   else
    echo -e "\n${redColour}[!]${endColour}${redColour} Error en la instalación de Apache.${endColour}"
    sleep 2
+	 tput cnorm
    error
   fi
 }
 
 function apachePostInstall() {
   clear
+	tput civis
   echo -e "\n${yellowColour}[*]${endColour}${grayColour} Ajustando Permisos.${endColour}"
   sleep 2
   cd /var/www/
@@ -70,32 +75,39 @@ function apachePostInstall() {
   usermod -aG www-data pi > /dev/null 2>/dev/null
   echo -e "\n${yellowColour}[*]${endColour}${grayColour} Proceso Correcto.${endColour}"
   sleep 2
+	tput cnorm
+	installPHP
 
 }
 
 function installPHP() {
   clear
+	tput civis
   echo -e "\n${yellowColour}[*]${endColour}${grayColour} Instalando PHP.${endColour}"
   sleep 2
   apt install -y php libapache2-mod-php php-mysql > /dev/null 2>/dev/null
   if [ $? -eq 0 ]; then
    echo -e "\n${yellowColour}[*]${endColour}${grayColour} PHP Instalado correctamente.${endColour}"
    sleep 2
+	 tput cnorm
    installMariaDB
   else
    echo -e "\n${redColour}[!]${endColour}${redColour} Error en la instalación de PHP.${endColour}"
    sleep 2
    error
   fi
+
 }
 function installMariaDB() {
   clear
+	tput civis
   echo -e "\n${yellowColour}[*]${endColour}${grayColour} Instalando MariaDB.${endColour}"
   sleep 2
   apt install -y mariadb-server > /dev/null 2>/dev/null
   if [ $? -eq 0 ]; then
    echo -e "\n${yellowColour}[*]${endColour}${grayColour} MariaDB Instalado correctamente.${endColour}"
    sleep 2
+	 tput cnorm
    configMariaDB
   else
    echo -e "\n${redColour}[!]${endColour}${redColour} Error en la instalación de MariaDB.${endColour}"
@@ -106,14 +118,18 @@ function installMariaDB() {
 
 function configMariaDB() {
   clear
+	tput civis
   echo -e "\n${yellowColour}[*]${endColour}${grayColour} Empezando con la configuración de MariaDB.${endColour}"
   sleep 1
   echo -e "\n\n${yellowColour}[*]${endColour}${grayColour} A continuación se van a configurar los permisos para un usuario que vamos a crear en MariaDB.${endColour}"
   sleep 1
+	tput cnorm
   echo -e "${yellowColour}[*]${endColour}${grayColour} Introduce el ${purpleColour}nombre de Usuario${endColour}${grayColour}.${endColour}"
   read userName
   echo -e "${yellowColour}[*]${endColour}${grayColour} Introduce la${purpleColour} contraseña${endColour}${grayColour} del usuario${endColour}${purpleColour} $userName${endColour}${grayColour}.${endColour}"
   read userPass
+
+	tput civis
 
   MYSQL=`which mysql`
   EXPECTED_ARGS=3
@@ -132,14 +148,17 @@ function configMariaDB() {
 
   echo -e "\n${yellowColour}[*]${endColour}${grayColour} Configuración de MariDB terminada.${endColour}"
   sleep 2
+	tput cnorm
   installPHPMyAdmin
 }
 
 function installPHPMyAdmin() {
   clear
+	tput civis
   echo -e "\n${yellowColour}[*]${endColour}${grayColour} Instalando PHPMyAdmin.${endColour}"
   sleep 2
   apt install -y phpmyadmin php-mbstring php-gettext; > /dev/null 2>/dev/null
+	tput cnorm
   if [ $? -eq 0 ]; then
    echo -e "\n${yellowColour}[*]${endColour}${grayColour} PHPMyAdmin Instalado correctamente.${endColour}"
    sleep 2
@@ -152,7 +171,10 @@ function installPHPMyAdmin() {
 }
 function end() {
   clear
+	tput civis
   echo -e "\n${yellowColour}[*]${endColour}${grayColour} La instalación ha terminado.${endColour}"
+	sleep 5
+	tput cnorm
 }
 
 # Main function
